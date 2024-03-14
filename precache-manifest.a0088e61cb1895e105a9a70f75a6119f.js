4934700a706feb67d21dd0c09d108858f96fc056
MickeyWoW <mickey970718@gmail.com>
2024-03-15 02:42:21 +0800

    'init'

---
+++ precache-manifest.a0088e61cb1895e105a9a70f75a6119f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c76ab1ecd552d8dc43c7",
+    "url": "/static/css/main.34f4e430.chunk.css"
+  },
+  {
+    "revision": "c76ab1ecd552d8dc43c7",
+    "url": "/static/js/main.c76ab1ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "08721f3ac65898e97d86",
+    "url": "/static/css/2.345061a0.chunk.css"
+  },
+  {
+    "revision": "08721f3ac65898e97d86",
+    "url": "/static/js/2.08721f3a.chunk.js"
+  },
+  {
+    "revision": "16ed46a8544fe9fa1437646665c8f235",
+    "url": "/static/media/github.16ed46a8.png"
+  },
+  {
+    "revision": "a326448d912f62e41f1d95e60793ec1d",
+    "url": "/static/media/ilyahacker.a326448d.svg"
+  },
+  {
+    "revision": "5a8a44becefc324713acd912feefc88d",
+    "url": "/static/media/song.5a8a44be.mp3"
+  },
+  {
+    "revision": "034de38c65e202c1cc838e7d014385fd",
+    "url": "/static/media/terminal.034de38c.ttf"
+  },
+  {
+    "revision": "227c08ad9458881bf70fc107cb1e4ce8",
+    "url": "/index.html"
+  }
+];
